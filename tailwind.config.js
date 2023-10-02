1864370e1d188dcaac89a471e851cfdcfc15caaa
Clark Wimberly <clark@clarklab.net>
2023-10-01 22:18:45 -0500

    lotsa blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,29 @@ module.exports = {
     "./**/*.{html,js,jsx}"
     ],
   theme: {
-    extend: {},
-      colors: {
-        'black': '#000000',
-        'blue': '#001aff',
-        'gray': '#555555',
-        'gray-on-yellow': '#d8d9ab',
-        'light': '#fafafa',
-        'light-gray': '#d9d9d9',
-        'light-green': '#ecfec7',
-        'neon-yellow': '#faff00',
-        'orange': '#ff4019',
-        'pink': '#ff2dd1',
-        'white': '#ffffff',
-        'yellow': '#feffc9',
-        'purple': '#7000ff',
-        'purple-700': '#3c0088',
-        'purple-900': '#270b4a',
-      }
+    colors: {
+      'black': '#000000',
+      'blue': '#001aff',
+      'gray': '#555555',
+      'gray-on-yellow': '#d8d9ab',
+      'light': '#fafafa',
+      'light-gray': '#d9d9d9',
+      'light-green': '#ecfec7',
+      'neon-yellow': '#faff00',
+      'orange': '#ff4019',
+      'pink': '#ff2dd1',
+      'white': '#ffffff',
+      'yellow': '#feffc9',
+      'purple': '#7000ff',
+      'purple-700': '#3c0088',
+      'purple-900': '#270b4a',
+      'gold': "#FFC700"
+    },
+    extend: {
+        borderColor: {
+            DEFAULT: '#d9d9d9',
+        }
+     }
   },
   plugins: [],
 }
